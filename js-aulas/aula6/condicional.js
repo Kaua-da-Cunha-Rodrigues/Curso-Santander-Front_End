abec44e836a4069ea874c52f6e772f66dd70da54
Kaua-da-Cunha-Rodrigues <cunhakaua2@gmail.com>
2023-11-15 17:01:06 -0300

    aula 6

---
+++ js-aulas/aula6/condicional.js
@@ -0,0 +1,17 @@
+const idade = 19
+
+if(idade >=18){
+    console.log("Você é maior de idade")
+}else{
+    console.log("Você é menor de idade")
+}
+
+const media = 5
+
+if(media >=7){
+    console.log("Aprovado")
+}else if(media >=5){
+    console.log("Recuperação")
+}else{
+    console.log("Reprovado")
+}
---
+++ js-aulas/aula6/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+const media = 5
+
+if(media >=7){
+    console.log("Aprovado")
+}else if(media >=5){
+    console.log("Recuperação")
+}else{
+    console.log("Reprovado")
+}
