130586495a7cfa7c189e3857a6f4e1e0b879ca31
Kaua-da-Cunha-Rodrigues <cunhakaua2@gmail.com>
2023-11-15 18:42:46 -0300

    aula 3

--- meu-projeto-react/src/App.jsx
+++ meu-projeto-react/src/App.jsx
@@ -1,9 +1,26 @@
-function App() {
+import MeuComponente from "./components/MeuComponente"
+
+function App() { //Primeiro componente da aplicação
 
 
   return (
-    <h1>Olá mundo, React!</h1> 
+    <div>
+
+      <h1>Olá mundo, React!</h1>
+      <MeuComponente/>
+
+      <MeuBotao conteudo="me clique"/>
+
+      <MeuBotao conteudo="depois aqui"/>
+
+      <MeuBotao conteudo="e por fim aqui"/>
+    </div> 
   )
 }
 
+function MeuBotao(props){ //é bom criar outros componentes em arquivos separados
+  return(
+    <button>{props.conteudo}</button> //pega o conteúdo escrito lá em cima
+  )
+}
 export default App
---
+++ meu-projeto-react/src/components/MeuComponente.jsx
@@ -0,0 +1,7 @@
+export default function MeuComponente(){ //Bom começar com maiúsculo
+    return(
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam qui odit tenetur maiores dignissimos voluptates ut provident ipsa iusto, dolore quos sapiente sequi cumque quo quidem natus laboriosam itaque! Quis?</p>
+    )
+}
+
+//export default la em cima está exportando
