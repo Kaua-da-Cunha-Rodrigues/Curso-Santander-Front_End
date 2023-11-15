5e75c500ac0661b242d8cf3827cd702ed0616436
Kaua-da-Cunha-Rodrigues <cunhakaua2@gmail.com>
2023-11-15 16:38:12 -0300

    aula 3

---
+++ js-aulas/aula3/operadores.js
@@ -0,0 +1,15 @@
+let numero1 = 20
+let numero2 = 30
+
+console.log("soma =", numero1 + numero2)
+console.log("subtração =", numero1 - numero2)
+console.log("multiplicação =", numero1 * numero2)
+console.log("divisão =", numero1 / numero2)
+console.log("exponencial =", numero1 ** numero2)
+console.log("resto divisão =", numero1 % 3)
+
+//Prioridade:
+
+//Biblioteca Math
+
+console(Math.PI)
