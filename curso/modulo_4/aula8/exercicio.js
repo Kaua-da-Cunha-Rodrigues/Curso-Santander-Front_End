b406c432530e9f05ecb9f6c1c0173298a6847607
Kaua-da-Cunha-Rodrigues <cunhakaua2@gmail.com>
2024-04-05 21:49:57 -0300

    aula 8 - módulo - Assunto: Fetch

---
+++ curso/modulo_4/aula8/exercicio.js
@@ -0,0 +1,28 @@
+/**
+ * Atividade
+ * 1) Crie uma função assíncrona que irá simular uma busca de usuários ao
+ * banco de dados.
+ * 1.1 - Essa função deve demorar 3 segundos (3000ms)
+ * 1.2 - A busca deve receber o parâmetro id para procurar no array de usuários
+ * 1.3 - Caso o usuário não seja encontrado, exibir uma mensagem de erro utilizando
+ * o catch
+ * 1.4 - Crie um contador e todas as vezes que a função de buscar usuário for chamada
+ * devemos incrementá-lo e logar na tela o número de chamadas
+ */
+
+/**
+ * 2) Crie uma função assíncrona que se chama buscarpostagens que irá simular
+ * as buscas das postagens do usuario por id no array;
+ * 2.1 - Caso o post ou id do usuário não seja encontrado, devemos mostrar a mensagem
+ * personalizada: "Usuário não encontrado" ou "Posts não encontrados para o usuário"
+ */
+
+/**
+ * 3) Crie uma função assíncrona chamda exibirDetalhesUsuário que irá utilizar as
+ * nossas duas funções assíncronas.
+ * 3-1 Nós devemos inserir o id do usuário e mostraremos todas as postagens dele
+ * e todos os seus dados.
+ */
+async function buscarUsuario(id){
+    setTimeout
+}
---
+++ curso/modulo_4/aula8/fetch/fetch.js
@@ -0,0 +1,6 @@
+//Fetch é uma API que faz requisições a aplicações externas para retornar resultados que desejamos. Quando ele faz a requisição a algo externo, ele retorna um type "cors" que é usado para importar algo
+
+fetch("https://google.com")
+.then((result) =>{
+
+})
